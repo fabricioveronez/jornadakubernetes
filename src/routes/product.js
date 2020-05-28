9c29c0b59551daab82cffb537a8ca985eef84c4a
Fabricio Veronez <fabricioveronez@gmail.com>
2020-05-28 15:56:09 -0300

    Versão final

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 var product_controller = require('../controllers/product');
 
-router.post('/', product_controller.productCreate);
-router.get('/', product_controller.productAll);
+router.post('', product_controller.productCreate);
+router.get('', product_controller.productAll);
 router.get('/:id', product_controller.productDetails);
 router.put('/:id', product_controller.productUpdate);
 router.delete('/:id', product_controller.productDelete);
